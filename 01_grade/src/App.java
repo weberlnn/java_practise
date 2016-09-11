ca69c5c2797f4af18be62dd770d142b8155e6dcb
weberlnn <1549017997@qq.com>
2016-09-11 10:15:09 +0800

    增加函数 evaluate 以便对分数评价等级

--- 01_grade/src/App.java
+++ 01_grade/src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    static public void main(String[] args) {
-        System.out.println(Grade.getSum(1, 3));
+    static public void main(String args[]) {
+        System.out.println(Grade.evaluate(90));
     }
 }
--- 01_grade/src/Grade.java
+++ 01_grade/src/Grade.java
@@ -1,5 +1,15 @@
 public class Grade {
-    static int getSum(int a, int b) {
-        return a + b;
+
+    public static String evaluate(int marks) {
+        if (marks >= 90) {
+            return "A";
+        } else if (marks >= 80) {
+            return "B";
+        } else if (marks >= 70) {
+            return "C";
+        } else {
+            return "D";
+        }
     }
 }
+
